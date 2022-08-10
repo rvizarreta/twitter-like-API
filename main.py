9bc9d938769d74f4382e2206f61e080d991c8fe1
Renzo Vizarreta <34606228+rvizarreta@users.noreply.github.com>
2022-08-10 18:26:56 -0500

    tweet_post

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ class Tweet(BaseModel):
         min_length=1,  
         max_length=256)
     created_at : datetime = Field(default = datetime.now())
-    update_at : Optional[datetime] = Field(default=None)
+    updated_at : Optional[datetime] = Field(default=None)
     by : User = Field(...)
 
 ############################################################
@@ -89,8 +89,35 @@ def home():
     status_code = status.HTTP_201_CREATED,
     summary = 'Post a tweet',
     tags = ['Tweets'])
-def post():
-    pass
+def post(tweet: Tweet = Body(...)):
+    """
+    Post a tweet
+
+    This path operation posts a tweet in the app.
+
+    Parameters:
+        - Request body parameter.
+            - tweet : Tweet
+
+    Returns a json with the basic tweet information:
+        - tweet_id : UUID
+        - content : str
+        - created_at : datetime
+        - updated_at : Optional[datetime]
+        - by : User
+    """
+    with open('tweets.json', 'r+', encoding='utf-8') as f:
+        results = json.loads(f.read())
+        tweet_dict = tweet.dict()
+        tweet_dict['tweet_id'] = str(tweet_dict['tweet_id'])
+        tweet_dict['created_at'] = str(tweet_dict['created_at'])
+        tweet_dict['updated_at'] = str(tweet_dict['updated_at'])
+        tweet_dict['by']['user_id'] = str(tweet_dict['by']['user_id'])
+        tweet_dict['by']['birth_date'] = str(tweet_dict['by']['birth_date'])
+        results.append(tweet_dict)
+        f.seek(0)
+        f.write(json.dumps(results))
+        return tweet
 
 ### Show a tweet
 @app.get(
