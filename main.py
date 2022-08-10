9a5187867843efd759c0cfdbd5e6f0dfb7c43a47
Renzo Vizarreta <34606228+rvizarreta@users.noreply.github.com>
2022-08-10 12:57:35 -0500

    models_users

---
+++ main.py
@@ -0,0 +1,53 @@
+# Python
+from lib2to3.pytree import Base
+from uuid import UUID
+from datetime import date
+from typing import Optional
+
+# Pydantic
+from uuid import UUID
+from pydantic import BaseModel
+from pydantic import EmailStr
+from pydantic import Field
+
+# FastAPI
+from fastapi import FastAPI
+
+app = FastAPI()
+
+############################################################
+# MODELS
+# ----------------------------------------------------------
+class UserBase(BaseModel):
+    user_id : UUID = Field(...)
+    email : EmailStr = Field(...)
+
+class UserLogin(UserBase):
+    password : str = Field(
+        ...,
+        min_length = 8
+    )
+
+class User(UserBase):
+    first_name : str = Field(
+        ...,
+        min_length = 1,
+        max_length=50
+    )
+    last_name : str = Field(
+        ...,
+        min_length = 1,
+        max_length=50
+    )
+    birth_date : Optional[date] = Field(default=None)
+
+class Tweet(BaseModel):
+    pass
+
+############################################################
+
+
+
+@app.get(path="/")
+def home():
+    return {"Twitter API" : "Working!"}
