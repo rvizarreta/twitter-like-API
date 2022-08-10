fd853b34830542e94bacd84975133108f8cf33de
Renzo Vizarreta <34606228+rvizarreta@users.noreply.github.com>
2022-08-10 17:28:25 -0500

    path_operations_structure_users

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from lib2to3.pytree import Base
 from uuid import UUID
 from datetime import date, datetime
-from typing import Optional
+from typing import Optional, List
 
 # Pydantic
 from uuid import UUID
@@ -12,6 +12,7 @@ from pydantic import Field
 
 # FastAPI
 from fastapi import FastAPI
+from fastapi import status
 
 app = FastAPI()
 
@@ -54,8 +55,58 @@ class Tweet(BaseModel):
 
 ############################################################
 
-
-
+############################################################
+# PATH OPERATIONS
+# ----------------------------------------------------------
 @app.get(path="/")
 def home():
     return {"Twitter API" : "Working!"}
+
+# ----------------------------------------------------------
+# USERS
+# ----------------------------------------------------------
+@app.post(
+    path = '/signup',
+    response_model = User,
+    status_code = status.HTTP_201_CREATED,
+    summary = 'Register a User',
+    tags = ['Users'])
+def signup():
+    pass
+
+@app.post(
+    path = '/login',
+    response_model = User,
+    status_code = status.HTTP_200_OK,
+    summary = 'Login a User',
+    tags = ['Users'])
+def login():
+    pass
+
+@app.get(
+    path = '/users',
+    response_model = List[User],
+    status_code = status.HTTP_200_OK,
+    summary = 'Show all users',
+    tags = ['Users'])
+def show_all_users():
+    pass
+
+@app.get(
+    path = '/users/{user_id}',
+    response_model = User,
+    status_code = status.HTTP_200_OK,
+    summary = 'Show a User',
+    tags = ['Users'])
+def show_a_user():
+    pass
+
+@app.delete(
+    path = '/users/{user_id}/delete',
+    response_model = User,
+    status_code = status.HTTP_200_OK,
+    summary = 'Delete a User',
+    tags = ['Users'])
+def delete_a_user():
+    pass
+############################################################
